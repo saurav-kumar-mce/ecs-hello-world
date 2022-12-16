18b8cbc31da012ab234234bcb100bccafdc9c7cc
saurav-kr <114326000+saurav-kumar-mce@users.noreply.github.com>
2022-12-16 09:33:54 +0530

    commit

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ terraform {
 }
 provider "aws" {
   region     = "ap-northeast-1"
-  access_key = "AKIA4WHVUN47QYKPX25S"
-  secret_key = "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"
+ # add- access_key = ""
+ # add- secret_key = ""
 
   default_tags {
     tags = {
